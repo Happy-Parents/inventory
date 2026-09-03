0c10ebe913df3c9a23bb9c438a5133a503594c3c
Kyryl Oliinyk <kyryloliinyk@gmail.com>
2026-09-03 19:39:09 +0300

    Close website for robots index

---
+++ app/controllers/robots_controller.rb
@@ -0,0 +1,18 @@
+class RobotsController < ActionController::Base
+  def show
+    render plain: Rails.env.production? ? DISALLOW_ALL : ALLOW_ALL,
+           content_type: "text/plain"
+  end
+
+  DISALLOW_ALL = <<~TXT
+    # See https://www.robotstxt.org/robotstxt.html for documentation on how to use the robots.txt file
+    User-agent: *
+    Disallow: /
+  TXT
+
+  ALLOW_ALL = <<~TXT
+    # See https://www.robotstxt.org/robotstxt.html for documentation on how to use the robots.txt file
+    User-agent: *
+    Allow: /
+  TXT
+end
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -18,6 +18,11 @@ Rails.application.configure do
   # Cache assets for far-future expiry since they are all digest stamped.
   config.public_file_server.headers = { "cache-control" => "public, max-age=#{1.year.to_i}" }
 
+  # Keep the site out of search engines.
+  config.action_dispatch.default_headers = config.action_dispatch.default_headers.merge(
+    "X-Robots-Tag" => "noindex, nofollow, noarchive, nosnippet"
+  )
+
   # Enable serving of images, stylesheets, and JavaScripts from an asset server.
   # config.asset_host = "http://assets.example.com"
 
