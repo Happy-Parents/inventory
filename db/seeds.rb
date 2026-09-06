bb3b40a26ff5bdd664f59ef458f7132ddb104e05
Kyryl Oliinyk <kyryloliinyk@gmail.com>
2026-09-06 19:53:45 +0300

    Move seeds to credentials

--- db/seeds.rb
+++ db/seeds.rb
@@ -1,7 +1,6 @@
-if Rails.env.development?
-  Admin.find_or_create_by!(email: "happyparentsua@gmail.com") do |admin|
-    admin.role = Admin.roles[:super_admin]
-    admin.password = "mubwat-syzmoJ-3jisto"
-    admin.password_confirmation = "mubwat-syzmoJ-3jisto"
-  end
+admin_creds = Rails.application.credentials.dig(:default_superadmin)
+Admin.find_or_create_by!(email: admin_creds[:email]) do |admin|
+  admin.role = Admin.roles[:super_admin]
+  admin.password = admin_creds[:password]
+  admin.password_confirmation = admin_creds[:password]
 end
